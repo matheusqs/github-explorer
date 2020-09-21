b69a2ee93673ac974be422d6bfe31a3f624c2d7d
Matheus Quintão <matheus.quintao.santiago@gmail.com>
2020-09-21 00:18:51 -0300

    Finaliza aplicacao

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,67 +1,101 @@
-import React from "react";
+import React, { useState, useEffect, FormEvent } from "react";
 import { FiChevronRight } from "react-icons/fi";
+import { Link } from "react-router-dom";
+import api from "../../services/api";
 
 import logoImg from "../../assets/logo.svg";
 
-import { Title, Form, Repositories } from "./styles";
+import { Title, Form, Error, Repositories } from "./styles";
+
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
 
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState("");
+  const [inputError, setInputError] = useState("");
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const storagedRepositories = localStorage.getItem(
+      "@GithubExplorer::repositories"
+    );
+
+    if (storagedRepositories) {
+      return JSON.parse(storagedRepositories);
+    }
+
+    return [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem(
+      "@GithubExplorer::repositories",
+      JSON.stringify(repositories)
+    );
+  }, [repositories]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>
+  ): Promise<void> {
+    event.preventDefault();
+
+    if (!newRepo) {
+      setInputError("Digite o autor/nome do repositório");
+      return;
+    }
+
+    try {
+      const response = await api.get<Repository>(`repos/${newRepo}`);
+
+      const repository = response.data;
+
+      setRepositories([...repositories, repository]);
+      setNewRepo("");
+      setInputError("");
+    } catch (err) {
+      setInputError("Erro na busca do repositório");
+    }
+  }
+
   return (
     <>
       <img src={logoImg} alt="Github Explorer" />
       <Title>Explore Repositórios no Github</Title>
 
-      <Form>
-        <input placeholder="Digite o nome do repositório" />
+      <Form hasError={!!inputError} onSubmit={handleAddRepository}>
+        <input
+          placeholder="Digite o nome do repositório"
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
+      {inputError && <Error>{inputError}</Error>}
+
       <Repositories>
-        <a href="teste">
-          <img
-            src="https://avatars2.githubusercontent.com/u/23320229?s=460&v=4"
-            alt="Matheus Santiago"
-          />
+        {repositories.map((repository) => (
+          <Link
+            key={repository.full_name}
+            to={`/repositories/${repository.full_name}`}
+          >
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
 
-          <div>
-            <strong>rocketseat/unform</strong>
-            <p>
-              Easy peasy highly scalable ReactJS & React Native forms! rocket
-            </p>
-          </div>
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-          <FiChevronRight />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars2.githubusercontent.com/u/23320229?s=460&v=4"
-            alt="Matheus Santiago"
-          />
-
-          <div>
-            <strong>rocketseat/unform</strong>
-            <p>
-              Easy peasy highly scalable ReactJS & React Native forms! rocket
-            </p>
-          </div>
-
-          <FiChevronRight />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars2.githubusercontent.com/u/23320229?s=460&v=4"
-            alt="Matheus Santiago"
-          />
-
-          <div>
-            <strong>rocketseat/unform</strong>
-            <p>
-              Easy peasy highly scalable ReactJS & React Native forms! rocket
-            </p>
-          </div>
-
-          <FiChevronRight />
-        </a>
+            <FiChevronRight />
+          </Link>
+        ))}
       </Repositories>
     </>
   );
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,7 +7,7 @@ import Repository from "../pages/Repository";
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Dashboard}></Route>
-    <Route path="/repository" component={Repository}></Route>
+    <Route path="/repositories/:repository+" component={Repository}></Route>
   </Switch>
 );
 
